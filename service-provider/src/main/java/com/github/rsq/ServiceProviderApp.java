c2f24fa02c977360fe5500832e201b11e50fc238
raoshaoquan <32005235@qq.com>
2017-09-18 19:04:59 +0800

    config-server示例

--- service-provider/src/main/java/com/github/rsq/ServiceProviderApp.java
+++ service-provider/src/main/java/com/github/rsq/ServiceProviderApp.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * Created by yangjunming on 2017/6/17.
  */
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceProviderApp {
 
     public static void main(String[] args) {
--- service-provider/src/main/java/com/github/rsq/controller/ComputeController.java
+++ service-provider/src/main/java/com/github/rsq/controller/ComputeController.java
@@ -3,25 +3,25 @@ package com.github.rsq.controller;
 /**
  * Created by raoshaoquan on 2017/7/11.
  */
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RefreshScope
 public class ComputeController {
 
-    @Autowired
-    private DiscoveryClient client;
+    @Value("${profile}")
+    private String profile;
 
-    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
+    }
+
+    @GetMapping(value = "/add")
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
-        System.out.println("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
     }
 
